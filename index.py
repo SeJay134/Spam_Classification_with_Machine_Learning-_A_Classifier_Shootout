11a5b3cf5202e0ab45b04aeca96b375858296dfc
SeJay134 <patrushev.s.job@gmail.com>
2026-06-19 17:53:12 -0400

    load

--- index.py
+++ index.py
@@ -24,3 +24,65 @@ from sklearn.metrics import (
 from sklearn.inspection import DecisionBoundaryDisplay
 
 warnings.filterwarnings("ignore", category=RuntimeWarning)
+
+path = 'resources/spambase.data'
+path_col_names = 'resources/spambase.names'
+
+try:
+    if os.path.exists(path):
+        df = pd.read_csv(path, header=None)
+        
+        print(df.head()) # col 58
+    else:
+        print(f"File does not exist: {path}")
+except FileNotFoundError as e:
+    print(f"File not found: {e}")
+except Exception as e:
+    print(f"Unexpected error: {e}")
+
+print(df.shape) # (4601, 58)
+
+col_names = []
+with open(path_col_names) as f:
+    for i in f:
+        i = i.strip()
+        if ':' in i and not i.startswith('|'):
+            name_of_col = i.split(':')[0]
+            col_names.append(name_of_col)
+print(len(col_names)) # 57
+# print(col_names)
+
+col_names.append('spam') # col N58
+df.columns = col_names
+
+print(len(col_names))   # 58
+# print(df.columns)
+print(df.head())
+
+spam = df[df['spam'] == 1]
+ham = df[df['spam'] == 0]
+
+plt.boxplot([ham['word_freq_free'], spam['word_freq_free']], labels=['ham', 'spam'])
+plt.title('word_freq_free: ham vs spam')
+plt.ylabel('Frequency')
+plt.grid(True)
+plt.savefig('outputs/word_freq_free.png')
+plt.close()
+plt.show()
+
+plt.boxplot([ham['char_freq_!'], spam['char_freq_!']], labels=['ham', 'spam'])
+plt.title('char_freq_!: ham vs spam')
+plt.ylabel('Frequency')
+plt.grid(True)
+plt.savefig('outputs/char_freq_!.png')
+plt.close()
+plt.show()
+
+plt.boxplot([ham['capital_run_length_total'], spam['capital_run_length_total']], labels=['ham', 'spam'])
+plt.ylabel('Frequency')
+plt.grid(True)
+plt.title('capital_run_length_total')
+plt.savefig('outputs/capital_run_length_total.png')
+plt.close()
+plt.show()
+
